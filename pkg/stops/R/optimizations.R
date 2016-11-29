c5f293ccbb984a390a71d3ae9693e846f2ad412b
trusch (Thomas Rusch) <thomas.rusch@wu-wien.ac.at>
2016-11-29 16:50:59 +0000

    changed tgp optim to also list real function value and also chooses optimum from it
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stops@189 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stops/R/optimizations.R
+++ pkg/stops/R/optimizations.R
@@ -116,8 +116,9 @@ ljoptim <- function(x,fun,...,red=ifelse(adaptive,0.99,0.95),lower,upper,acc=1e-
 #' 
 #' @return A list with the components (for compatiility with \code{\link{optim}})
 #' \itemize{
-#'      \item par The position of the optimimum in the search space (parameters that minimize the function; argmin fun)
-#'      \item value The value of the objective function at the optimum (min fun)
+#'      \item par The position of the optimum in the search space (parameters that minimize the function; argmin fun). 
+#'      \item value The value of the objective function at the optimum (min fun). Note we do not use the last value in the candidate list but the best candidate (which can but need not coincide). 
+#'      \item svalue The value of the surrogate objective function at the optimal parameters
 #'      \item counts The number of iterations performed at convergence with entries fnction for the number of iterations and gradient which is always NA at the moment
 #'      \item convergence 0 successful completion by the accd or acc criterion, 1 indicate iteration limit was reached, 99 is a problem 
 #'      \item message is NULL (only for compatibility or future use)
@@ -138,10 +139,10 @@ ljoptim <- function(x,fun,...,red=ifelse(adaptive,0.99,0.95),lower,upper,acc=1e-
 #' res1<-tgpoptim(c(-1.2,1),fbana,lower=c(-5,-5),upper=c(5,5),acc=1e-16,itmax=100)
 #' res1
 #'
-#' set.seed(210485)
 #' fwild <- function (x) 10*sin(0.3*x)*sin(1.3*x^2) + 0.00001*x^4 + 0.2*x+80
+#' set.seed(210485)
 #' plot(fwild, -50, 50, n = 1000, main = "Bayesian GP Optimization minimizing 'wild function'")
-#' res2<-tgpoptim(50, fwild,lower=-50,upper=50,adaptive=FALSE,acc=1e-16,itmax=100,model=bgp)
+#' res2<-tgpoptim(50, fwild,lower=-50,upper=50,adaptive=FALSE,acc=1e-16,itmax=100,model="bgp")
 #' points(res2$par,res2$value,col="red",pch=19)
 #' res2
 #' }
@@ -156,7 +157,7 @@ tgpoptim <- function(x,fun,...,initpoints=10,lower,upper,acc=1e-8,itmax=10,verbo
         X <- tgp::dopt.gp(initpoints-1,X=x,Xcand)$XX
         X <- rbind(x,X)
         if(verbose>0) cat("Evaluating function at initial points","\n")
-        Z <- apply(X, 1, fun)
+        Z <- apply(X, 1, fun) #Capital Z ist real function value, small z is surrogate function value
         out <- progress <-  NULL
         convo <- 99L
         itel <- 1
@@ -168,11 +169,14 @@ tgpoptim <- function(x,fun,...,initpoints=10,lower,upper,acc=1e-8,itmax=10,verbo
           out <- tgp::optim.step.tgp(fun, X=X, Z=Z, rect=rect, model=model, prev=out,verb=verbose-3) 
           ## add in the inputs, and newly sampled outputs
           X <- rbind(X, out$X)
-          ztmp <- apply(out$X,1,fun)
-          Z <- c(Z, ztmp)
+          Ztmp <- apply(out$X,1,fun)
+          Z <- c(Z, Ztmp)
           ## keep track of progress and optimum
-          fnew <- out$progress$z 
-          progress <- rbind(progress, out$progress)
+          sfnew <- out$progress$z #candidate point for optimum from surrogate function 
+          fnew <- apply(out$progress[1,seq.int(1,dim(X)[2]),drop=FALSE],1,fun) #real function value at surrogate optimum
+          out$progress$Z <- fnew 
+          progress <- rbind(progress, out$progress) #progress of optimum from surrogate #TODO: extend this by real value?
+#          progress <- rbind(progress,out$progress)
           if(verbose>1) print(progress[itel,])
           if ((itel == itmax) || (abs(fold - fnew) < acc)) {
             convo <- 0L
@@ -182,7 +186,10 @@ tgpoptim <- function(x,fun,...,initpoints=10,lower,upper,acc=1e-8,itmax=10,verbo
           itel <- itel + 1
           fold <- fnew
         }
-        rets <- progress[which.min(progress$z),1:length(x)]
-        return(list(par=as.numeric(rets),value=min(progress$z),counts=c(`function`=itel,gradient=NA),convergence=convo,message=NULL,history=progress,tgpout=out))
+        rets <- progress[which.min(progress$Z),1:length(x)] #we use the best candidate of all, needs not be the last nor necessarily the same as the best surrogate value 
+        #rets <- progress[itel,1:length(x)] #last surrogate function candidate
+        valsorig <- fnew #real function value at best candidate
+       # valsorig <- NULL
+        return(list(par=as.numeric(rets),value=min(progress$Z),svalue=progress$z[which.min(progress$Z)],counts=c(`function`=itel,gradient=NA),convergence=convo,message=NULL,history=progress,tgpout=out))
 }
 
