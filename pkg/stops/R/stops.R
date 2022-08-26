1a162834c14de4087cc9ebaa9087ed74940a6401
trusch (Thomas Rusch) <thomas.rusch@wu-wien.ac.at>
2022-08-26 16:39:39 +0000

    added many new solvers from nloptr and cmaes and dfoptim]
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stops@346 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stops/R/stops.R
+++ pkg/stops/R/stops.R
@@ -1088,22 +1088,30 @@ mkPower2<-function(x,theta) {
 #' @param stressweight weight to be used for the fit measure; defaults to 1
 #' @param strucweight weight to be used for the cordillera; defaults to -1/length(structures)
 #' @param strucpars (possibly named with the structure). Metaparameters for the structuredness indices (gamma in the article). It's safest for it be a list of lists with the named arguments for the structuredness indices and the order of the lists must be like the order of structures. So something like this \code{list(list(par1Struc1=par1Struc1,par2Struc1=par2Struc1),list(par1Struc2=par1Struc2,par2Struc2=par2Struc2),...)} where parYStrucX are the named arguments for the metaparameter Y of the structure X the list elements corresponds to. For a structure without parameters, set NULL. Parameters in different list elements parYStrucX can have the same name. For example, say we want to use cclusteredness with metaparameters epsilon=10 and k=4 (and the default for the other parameters), cdependence with no metaparameters and cfaithfulness with metaparameter k=7 one would \code{list(list(epsilon=10,k=4),list(NULL),list(dis=obdiss,k=6))}  for structures vector ("cclusteredness","cdependence","cfaithfulness"). The parameter lists must be in the same ordering as the indices in structures. If missing it is set to NULL and defaults are used. It is also possible to supply a structure's metaparameters as a list of vectors with named elements if the metaparameters are scalars, so like \code{list(c(par1Struc1=parStruc1,par2Struc1=par1Struc1,...),c(par1Struc2=par1Struc2,par2Struc2=par2Struc2,...))}. That can have unintended consequences if the metaparameter is a vector or matrix.  
-#' @param optimmethod What optimizer to use (all support box contraints). Currently supported are Bayesian optimization with Gaussian Process priors and Kriging ("Kriging"), Bayesian optimization with treed Gaussian processes with jump to linear models ("tgp"), Adaptive LJ Search ("ALJ"), Particle Swarm optimization ("pso"), simulated annealing ("SANN"). Defaults to "tgp" version.
+#' @param optimmethod What solver to use. Currently supported are Bayesian optimization with Gaussian Process priors and Kriging ("Kriging"), Bayesian optimization with treed Gaussian processes with jump to linear models ("tgp"), Adaptive LJ Search ("ALJ"), Particle Swarm optimization ("pso"), simulated annealing ("SANN"), "DIRECT", Stochastic Global Optimization ("stogo"), COBYLA ("cobyla"), Controlled Random Search 2 with local mutation ("crs2lm"), Improved Stochastic Ranking Evolution Strategy ("isres"), Multi-Level Single-Linkage ("mlsl"), Nelder-Mead ("neldermead"), Subplex ("sbplx"), Hooke-Jeeves Pattern Search ("hjk"), CMA-ES ("cmaes"). Defaults to "ALJ" version. tgp, ALJ, Kriging and pso usually work well for low values of itmax. 
 #' @param lower The lower contraints of the search region. Needs to be a numeric vector of the same length as the parameter vector theta. 
 #' @param upper The upper contraints of the search region. Needs to be a numeric vector of the same length as the parameter vector theta.  
-#' @param verbose numeric value hat prints information on the fitting process; >2 is pretty verbose.
+#' @param verbose numeric value hat prints information on the fitting process; >2 is very verbose.
 #' @param type which aggregation for the multi objective target function? Either 'additive' (default) or 'multiplicative'
-#' @param itmax maximum number of iterations of the outer optimization (for theta); number of steps of Bayesian optimization if Kriging or tgp is used; default is 50. We recommend a higher number for ALJ (around 150). Note that with tgp the actual number of evaluation of the MDS method is between itmax and 6*itmax as tgp it samples 1-6 candidates from the posterior and uses the best candidate. For pso it is the number of particles s times itmax. 
-#' @param itmaxps maximum number of iterations of the inner optimization (to obtain the configuration)
+#' @param itmax maximum number of iterations of the outer optimization (for theta) or number of steps of Bayesian optimization; default is 50. We recommend a higher number for ALJ (around 150). Note that due to the inner workings of some solvers, this may or may not correspond to the actual number of function evaluations performed (or PS models fitted). E.g., with tgp the actual number of function evaluation of the PS method is between itmax and 6*itmax as tgp samples 1-6 candidates from the posterior and uses the best candidate. For pso it is the number of particles s times itmax. For cmaes it is usually a bit higher than itmax. This currently may get overruled by a control argument if it is used (and then set to either ewhat is supplie dby control or to the default of the method).    
+#' @param itmaxps maximum number of iterations of the inner optimization (to obtain the PS configuration)
 #' @param initpoints number of initial points to fit the surrogate model for Bayesian optimization; default is 10.
 #' @param model a character specifying the surrogate model to use. For Kriging it specifies the covariance kernel for the GP prior; see \code{\link{covTensorProduct-class}} defaults to "powerexp". For tgp it specifies the non stationary process used see \code{\link{bgp}}, defaults to "btgpllm" 
-#' @param control a control argument passed to the outer optimization procedure (see pomp::sannbox for SANN and pso::psoptim for pso).
-#' @param ... additional arguments passed to the outer optimization procedures (not fully tested)
+#' @param control a control argument passed to the outer optimization procedure. Will override any other control arguents passed, especially verbose and itmax. For the efect of control, see the functions pomp::sannbox for SANN and pso::psoptim for pso, cmaes::cma_es for cmaes, dfoptim::hjkb for hjk and the nloptr docs for the algorithms DIRECT, stogo, cobyla, crs2lm, isres, mlsl, neldermead, sbplx.
+#' @param ... additional arguments passed to the outer optimization procedures (not fully tested).
 #
 #'@return A list with the components
 #'         \itemize{
-#'         \item stoploss: the weighted loss value
-#'         TBD
+#'         \item stoploss: the stoploss value
+#'         \item optim: the object returned from the optimization procedure
+#'         \item stressweight: the stressweight
+#'         \item strucweight: the vector of structure weights
+#'         \item call: the call
+#'         \item optimmethod: The solver selected
+#'         \item losstype: The PS badness-of-fit function
+#'         \item nobj: the number of objects in the configuration
+#'         \item type: The type of stoploss scalacrisation (additive or multiplicative)
+#'         \item fit: The fitted PS object (most importantly $fit$conf the configuration and) 
 #' }
 #' 
 #' @examples
@@ -1130,11 +1138,14 @@ mkPower2<-function(x,theta) {
 #' @importFrom DiceKriging km
 #' @importFrom tgp lhs dopt.gp
 #' @importFrom pomp sannbox
+#' @importFrom nloptr direct stogo cobyla crs2lm isres mlsl neldermead sbplx
+#' @importFrom cmaes cma_es
+#' @importFrom dfoptim hjkb
 #' @import cordillera
 #' 
 #' @keywords clustering multivariate
 #' @export
-stops <- function(dis,loss=c("strain","stress","smacofSym","powerstress","powermds","powerelastic","powerstrain","elastic","sammon","sammon2","smacofSphere","powersammon","rstress","sstress","isomap","isomapeps","bcstress","lmds"), theta=1, structures=c("cclusteredness","clinearity","cdependence","cmanifoldness","cassociation","cnonmonotonicity","cfunctionality","ccomplexity","cfaithfulness","cregularity","chierarchy","cconvexity","cstriatedness","coutlying","cskinniness","csparsity","cstringiness","cclumpiness","cinequality"), ndim=2, weightmat=NULL, init=NULL, stressweight=1, strucweight, strucpars, optimmethod=c("SANN","ALJ","pso","Kriging","tgp"), lower, upper, verbose=0, type=c("additive","multiplicative"),initpoints=10,itmax=50,itmaxps=10000, model,control,...)
+stops <- function(dis,loss=c("strain","stress","smacofSym","powerstress","powermds","powerelastic","powerstrain","elastic","sammon","sammon2","smacofSphere","powersammon","rstress","sstress","isomap","isomapeps","bcstress","lmds"), theta=1, structures=c("cclusteredness","clinearity","cdependence","cmanifoldness","cassociation","cnonmonotonicity","cfunctionality","ccomplexity","cfaithfulness","cregularity","chierarchy","cconvexity","cstriatedness","coutlying","cskinniness","csparsity","cstringiness","cclumpiness","cinequality"), ndim=2, weightmat=NULL, init=NULL, stressweight=1, strucweight, strucpars, optimmethod=c("SANN","ALJ","pso","Kriging","tgp","DIRECT","stogo","cobyla","crs2lm","isres","mlsl","neldermead","sbplx","hjk","cmaes"), lower, upper, verbose=0, type=c("additive","multiplicative"),initpoints=10,itmax=50,itmaxps=10000, model, control,...)
     {
       #TODO add more transformations for the g() and f() by the transformation argument. We only use power versions right now, flexsmacof will allow for more (splines or a smoother or so)
       if(missing(structures)) {
@@ -1154,7 +1165,7 @@ stops <- function(dis,loss=c("strain","stress","smacofSym","powerstress","powerm
          strucweight <- rep(-1/length(structures),length(structures))
          if(verbose>1) cat("Weights are stressweight=",stressweight,"strucweights=", strucweight,"\n")
       }
-      if(missing(optimmethod)) optimmethod <- "tgp"  
+      if(missing(optimmethod)) optimmethod <- "ALJ"  
       if(verbose>0) cat("Starting Optimization \n ")
         if(optimmethod=="SANN") {
             ##NOTE: Used optim first, but that has no box constraints. Then tried all kinds but it all sucks.
@@ -1234,7 +1245,96 @@ stops <- function(dis,loss=c("strain","stress","smacofSym","powerstress","powerm
        thetaopt <- opt$par #parameters where best value found (we do not use the last one as that may be worse)
        bestval <- opt$value #best stoploss value
        itel <- opt$counts["function"] 
-       }
+    }
+     if(optimmethod=="DIRECT") {
+            if (verbose>1) cat("DIRECT Optimization","\n")
+            if(missing(control)) control <- list(maxeval=itmax)
+          opt<- nloptr::direct(function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,nl.info=isTRUE(all.equal(verbose-2,0)),control=control,...)
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$iter
+     }
+      if(optimmethod=="stogo") {
+          if (verbose>1) cat("StoGO Optimization","\n")
+          #cat(itmax,"\n")
+         #   if(missing(control)) control <- list(maxeval=itmax)  
+          opt<- nloptr::stogo(theta,function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,nl.info=isTRUE(verbose>2),maxeval=itmax,...)
+          #TODO Issue with maxeval?
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$iter
+      }
+         if(optimmethod=="cobyla") {
+            if (verbose>1) cat("COBYLA Optimization","\n")
+            if(missing(control)) control <- list(maxeval=itmax)  
+           opt<- nloptr::cobyla(theta,function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,nl.info=isTRUE(all.equal(verbose-2,0)),control=control,...)
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$iter
+         }
+        if(optimmethod=="crs2lm") {
+            if (verbose>1) cat("crs2lm Optimization","\n")
+            #if(missing(control)) control <- list(maxeval=itmax)  
+           opt<- nloptr::crs2lm(theta, function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,nl.info=isTRUE(verbose>3),maxeval=itmax,...)
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$iter
+        }
+        if(optimmethod=="isres") {
+            if (verbose>1) cat("isres Optimization","\n")
+            #if(missing(control)) control <- list(maxeval=itmax)  
+           opt<- nloptr::crs2lm(theta,function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,nl.info=isTRUE(verbose>3),maxeval=itmax,...)
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$iter
+        }
+        if(optimmethod=="mlsl") {
+            if (verbose>1) cat("MLSL Optimization","\n")
+            if(missing(control)) control <- list(maxeval=itmax)  
+           opt<- nloptr::mlsl(theta,function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,nl.info=isTRUE(verbose>3),control=control,...)
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$iter
+        }
+        if(optimmethod=="neldermead") {
+            if (verbose>1) cat("Nelder-Mead Optimization","\n")
+            if(missing(control)) control <- list(maxeval=itmax)  
+           opt<- nloptr::neldermead(theta,function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,nl.info=isTRUE(verbose>3),control=control,...)
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$iter
+        }
+          if(optimmethod=="sbplx") {
+            if (verbose>1) cat("Subplex Optimization","\n")
+            if(missing(control)) control <- list(maxeval=itmax)  
+           opt<- nloptr::sbplx(theta,function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,nl.info=isTRUE(verbose>3),control=control,...)
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$iter
+      }
+        if(optimmethod=="hjk") {
+            if (verbose>1) cat("Hooke-Jeeves Optimization","\n")
+            if(missing(control)) control <- list(info=isTRUE(all.equal(verbose-2,0)),maxfeval=itmax)
+          opt<- dfoptim::hjkb(theta, function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,control=control,...)
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$feval
+        }
+         if(optimmethod=="cmaes") {
+            if (verbose>1) cat("CMA-ES Optimization","\n")
+            theta <- as.vector(theta)
+            if(missing(control)) {
+                  #for calculating the correct number of calls to fn
+                  N <- length(theta)
+                  lambda <- 4 + floor(3 * log(N))
+                  control <- list(maxit=ceiling(itmax/lambda))
+                }
+          opt<- cmaes::cma_es(theta, function(theta) do.call(psfunc,list(dis=dis,theta=theta,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))$stoploss,lower=lower,upper=upper,control=control,...)
+            thetaopt <- opt$par
+            bestval <- opt$value
+            itel <- opt$counts[1]
+         }
+        #TODO: Streamline the number of function evaluations to be supplied returned for the different solvers. E.g. for cma_es it is itmax*population size. Or for tgp it is also itmax*6 or so. 
     #refit optimal model  
     out <- do.call(psfunc,list(dis=dis,theta=thetaopt,ndim=ndim,weightmat=weightmat,init=.confin,structures=structures,stressweight=stressweight,strucweight=strucweight,strucpars=strucpars,verbose=verbose-3,type=type,itmax=itmaxps))
     out$stoploss <- bestval
