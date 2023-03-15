52bd301157f13d547191f3e178accc7eab2934bc
trusch (Thomas Rusch) <thomas.rusch@wu-wien.ac.at>
2023-03-15 12:09:01 +0000

    fixed phidistance docs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stops@379 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cops/R/phidist.R
+++ pkg/cops/R/phidist.R
@@ -1,10 +1,10 @@
 #' Calculating the pairwise phi distance matrix between n vectors
 #'
-#' The pairwise distance of two vectors x and y is sqrt(sum(((x[i]-y[i])^2)(x[i]+y[i]))/(sum(x)+sum(y))). The function calculates this for any two rows of a matrix or data frame X   
+#' The pairwise phi-distance of two vectors x and y is sqrt(sum(((x[i]-y[i])^2)/((x[i]+y[i])*(sum(x)+sum(y))))). The function calculates this for all pairs of rows of a matrix or data frame X.   
 #'
 #' @param X an n times p numeric matrix or data frame 
 #'
-#' @return a symmetric n times n matrix of pairwise phi distance (betwen rows of X). Is an object of class distance. 
+#' @return a symmetric n times n matrix of pairwise phi distance (between rows of X) with 0 in the main diagonal. Is an object of class distance and matrix. 
 #' 
 #' @importFrom analogue distance
 #' 
@@ -12,7 +12,7 @@ phidistance <- function(X)
 {
       summ<- apply(X,1,sum)
       NN <- outer(summ,summ,"+")
-      disttemp <- analogue::distance(data,method="SQchi.square")
+      disttemp <- analogue::distance(X,method="SQchi.square")
       out <- disttemp/NN
       out <- sqrt(out)
       attr(out,"method") <- 'phi'
