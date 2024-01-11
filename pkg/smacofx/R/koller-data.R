628fa4bd1fd8ffa3e756b6e7f4e9a0b82a8ae464
trusch (Thomas Rusch) <thomas.rusch@wu-wien.ac.at>
2024-01-11 16:22:29 +0000

    added koller data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stops@498 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/smacofx/R/koller-data.R
@@ -0,0 +1,8 @@
+#' Responses to the SCC scale and CSII scale (n=1013). 
+#'
+#' 1013 respondents answered the items of the consumer susceptibility top interpersonal influence (CSII) and the self-concept clarity (SCC) scale. The answers are on a five-point-Likert scale with 1 meaning "fully agree" and 5 meaning "fully disagree". Note that on scc the 10th item is reversed coded to align with the other items.
+#'
+#' @format A 1013 x 24 data frame.  
+#' @source Koller, Floh, Zauner, Rusch (2013) "Persuasibility and the Self – Investigating Heterogeneity among Consumers". Australasian Marketing Journal, 21, 94-104. 
+#' @name koller
+NULL
